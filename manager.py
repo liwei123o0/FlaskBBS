6456689bf7ba2965e5a38f45b93d8bf21f852dc6
liwei <877129310@qq.com>
2015-09-10 10:57:26 +0800

    ONE_0.3

--- manager.py
+++ manager.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/env python
-from flask import Flask
+from flask import Flask,render_template
 from flask.ext.script import Manager
-
+from flask.ext.bootstrap import Bootstrap
 app = Flask(__name__)
+Bootstrap(app)
 
 manager = Manager(app)
 
-@manager.command
+
+@app.route('/')
 def index():
-    print "hello word! %s"
+    return 'hello'
+@app.route('/html')
+def html():
+    return render_template('/index.html')
 
 if __name__ =="__main__":
 
