4c29cd779dad092370f2643bf87518185bb0eb39
liwei <877129310@qq.com>
2015-09-10 18:00:16 +0800

    ONE_0.5

--- flaskboot/app/text.py
+++ flaskboot/app/text.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/env python
+from datetime import datetime
 
-from flask import Flask,render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ =='__main__':
-    app.run(debug=True)
+print datetime.utcnow()
